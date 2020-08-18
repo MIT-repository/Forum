2fbd9c1f672596700241bb9466dba835ac4e5dee
su0766 <32950118+su0766@users.noreply.github.com>
2020-08-18 22:41:15 +0900

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '96e354885ca3e986e456',
-  GITHUB_CLIENT_SECRET: 'd06603d4b1d4a39694c11fdc7cfc1cc13ed815cb',
-  GITHUB_CALLBACK_URL: 'http://101.101.211.186:8080/api/user/authViaGitHub/callback',
-  DBURL: 'mongodb://101.101.210.48:27017/reforum',
+  GITHUB_CLIENT_ID: '',
+  GITHUB_CLIENT_SECRET: '',
+  GITHUB_CALLBACK_URL: '',
+  DBURL: '',
 };
